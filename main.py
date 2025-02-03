a6e5d2e05bd49a5ebffecca189e91eb10c0c7acf
MEDLAO <sofiane.medhouny.laouina@gmail.com>
2025-02-03 15:31:52 +0100

    Add download option

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import io
 import qrcode
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Response
 from fastapi.responses import StreamingResponse
 
 
@@ -8,7 +8,7 @@ app = FastAPI()
 
 
 @app.get("/generate_qr/")
-def generate_qr(text: str = Query(..., description="Text or URL to encode in QR Code")):
+def generate_qr(text: str = Query(..., description="Text or URL to encode in QR Code"), download: bool = False):
     """Generates a QR code from the given text or URL."""
     qr = qrcode.make(text)
 
@@ -20,6 +20,13 @@ def generate_qr(text: str = Query(..., description="Text or URL to encode in QR 
 
     # Move the pointer back to the start of the buffer
     img_io.seek(0)
+
+    if download:
+        return Response(
+            content=img_io.getvalue(),
+            media_type="image/png",
+            headers={"Content-Disposition": f"attachment; filename=qr_code.png"}
+        )
 
     # Return the image as a response without saving it
     return StreamingResponse(img_io, media_type="image/png")
